344607fa25bff6c7d1acd21cd01b57839b151211
Scott <scott-huang@hotmail.com>
2015-02-01 00:11:06 -0500

    Started section 4.5

---
+++ app/src/androidTest/java/TipCalculator.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Scott on 2/1/2015.
+ */
+
+// TipCalculator.java
+// Calculates bills using 10, 15, 20 and custom percentage tips.
+package com.deitel.tipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class TipCalculator {
+}
